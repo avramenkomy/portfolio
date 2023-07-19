5cfd633ba62809710ce28ee1950263f547b62953
avramenkomy <avramenkomy@yandex.ru>
2023-07-19 16:11:38 +0300

    WIP: update imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,9 @@ import Layout from './Layout';
 import {
   NotFound, Home, About,
   Skills, Blog,
-  Contacts, Resume, Test,
+  Contacts, Resume, Test, Projects,
 } from './components';
 
-import Projects from './components/MiniProjects';
 import TodosApp from './components/MiniProjects/Todos';
 
 import AuthProvider from './components/hoc/AuthProvider';
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Skills } from './Skills';
-export { default as Projects } from './Projects';
+export { default as Projects } from './MiniProjects';
 export { default as Blog } from './Blog';
 export { default as Contacts } from './Contacts';
 export { default as Resume } from './Resume';
