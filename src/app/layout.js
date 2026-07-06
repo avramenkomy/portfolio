c7574465c9badec91afe94abaa462abfbbe6a227
avramenkomy <avramenkomy@yandex.ru>
2026-07-06 15:46:29 +0300

    feat: add site metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,20 +5,51 @@ import { Geist } from 'next/font/google';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
+import site from '@/data/site';
+
 const geist = Geist({
   subsets: ['latin'],
 });
 
 export const metadata = {
-  title: 'Mikhail Avramenko | Frontend Developer',
-  description: 'personal portfolio',
+  metadataBase: new URL(site.url),
+
+  title: {
+    default: site.title,
+    template: `%s | ${site.name}`
+  },
+  description: site.description,
+  applicationName: site.name,
+  authors: [
+    {
+      name: site.name,
+    }
+  ],
+  creator: site.name,
+  openGraph: {
+    type: 'website',
+    locale: site.locale,
+    url: site.url,
+    siteName: site.name,
+    title: site.title,
+    description: site.description,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: site.title,
+    description: site.description,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  }
 }
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body className={`${geist.className} bg-zinc-950 text-zinc-500`}>
+      <body className={`${geist.className} bg-zinc-950 text-zinc-100`}>
         <Header />
 
         <div className="pt-16">
---
+++ src/data/site.js
@@ -0,0 +1,10 @@
+const site = {
+  name: 'Mikhail Avramenko',
+  title: 'Mikhail Avramenko | Frontend Developer',
+  description:
+    'Frontend Developer building fast, modern and responsive web interfaces with JavaScript, TypeScript, React and Next.js',
+  url: 'http://localhost:3000', // TODO: change to domain name with vercel deploy
+  locale: 'ru-Ru',
+}
+
+export default site;
