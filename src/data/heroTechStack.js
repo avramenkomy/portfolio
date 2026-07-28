9dd1a571e1c562da73d7ffefe479a88e6bbc1d54
avramenkomy <avramenkomy@yandex.ru>
2026-07-28 10:35:59 +0300

    feature/add new skills

--- src/data/heroTechStack.js
+++ src/data/heroTechStack.js
@@ -1,5 +1,25 @@
 const heroTechStack = [
-  'JavaScript', 'TypeScript', 'React', 'Redux', 'Next.js', 'Tailwind CSS',
+  // frontend skills
+  'HTML',
+  'CSS', 'SCSS', 'SASS', 'LESS',
+  'Tailwind CSS', 'Material UI', 'Bootstrap', 'Ant Design',
+  'JavaScript', 'TypeScript',
+  'React',
+  'Redux', 'Redux Toolkit',
+  'MobX',
+  'Next.js', 'Auth.js/NextAuth',
+  'Jest', 'Vitest',
+  'Vite', 'Webpack',
+
+  // backend skills
+  'Python',
+  'Django', 'Django Rest Framework',
+
+  // Version Control
+  'Git',
+
+  // Geo
+  'MapLibre GL JS', 'OpenStreetMap',
 ]
 
 export default heroTechStack;
