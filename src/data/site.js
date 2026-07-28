52c3317c5059964db389ff6d1e58b0fa13e511be
avramenkomy <avramenkomy@yandex.ru>
2026-07-28 13:03:21 +0300

    chore: setup site url for production

--- src/data/site.js
+++ src/data/site.js
@@ -3,7 +3,7 @@ const site = {
   title: 'Mikhail Avramenko | Frontend Developer',
   description:
     'Frontend Developer building fast, modern and responsive web interfaces with JavaScript, TypeScript, React and Next.js',
-  url: 'https://avramenkomy.vercel.app',
+  url: process.env.NEXT_PUBLIC_SITE_URL || 'https://avramenkomy.vercel.app' || "http://localhost:3000", // 'https://avramenkomy.vercel.app',
   locale: 'ru-Ru',
 }
 
